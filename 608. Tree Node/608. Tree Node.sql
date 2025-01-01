98e2123d8e241ea5c6aecbc1f6af527e7e759971
Lili Zhou <lilizhou0215@gmail.com>
2025-01-01 10:02:54 -0500

    Create 608. Tree Node.sql

---
+++ 608. Tree Node/608. Tree Node.sql
@@ -0,0 +1,19 @@
+
+-- # logic
+root: no p_id 
+inner : has p_id and left join not null 
+leaf: has p_id and left join is null 
+
+-- Solution
+  
+select id, 'Root' as type from Tree where p_id is null
+
+union 
+
+select a.id, 
+case 
+  when b.p_id is null then 'Leaf'
+  when b.p_id is not null then 'Inner'
+end as type
+
+from Tree a left join Tree b on a.id = b.p_id where a.p_id is not null
