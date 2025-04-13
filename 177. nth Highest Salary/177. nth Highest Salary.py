d1b22a0279191369a68a81e1197813bcee24d6b7
Lili Zhou <lilizhou0215@gmail.com>
2025-04-13 12:53:01 -0400

    Update 177. nth Highest Salary.py

--- 177. nth Highest Salary/177. nth Highest Salary.py
+++ 177. nth Highest Salary/177. nth Highest Salary.py
@@ -1,12 +1,11 @@
 ''' 
 Problem: find the nth highest salary from the Employee table, if no nth highest, return null 
 
-Logic: add rank column, and then select rnk ==2 
 '''
 
-def nth_highest_salary(df,N:int):
-    df.drop_duplicates()
-    df['rnk'] = df['Salary'].rank(method = 'dense',ascending = False)
-    nth_salary = df[df['rnk']==2]['Salary']
-    result = pd.DataFrame({f'getNthHigestSalary({N})': [None] if nth_salary.empty else [nth_salary.iloc[0]]})
-    return result 
+ def nth_highest_salary(df: pd.DataFrame, n: int):
+    df['rnk'] = df['salary'].rank(method='dense', ascending=False)
+    salary_nth = df[df['rnk']==n].salary
+    value = salary_nth.iloc[0] if not salary_nth.empty else None
+    df_result = pd.DataFrame({f'getNthHighestSalary({n})': [value]})
+    return df_result
